ed157a8fdb8eecb056d82db2aba520ff3ec51f8b
Eliel Jales <eliel.jales@cortex-intelligence.com>
2024-07-22 16:40:55 -0300

    :fire: Definindo novo nome para versão

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pycortexintelligence",
-    version="1.2.3",
+    version="1.2.2-embrapa",
     author="Enderson Menezes",
     scripts=["cortex.py"],
     author_email="data.integrations@cortex-intelligence.com",
